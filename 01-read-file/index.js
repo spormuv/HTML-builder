a6cad016eee3feb6c3c948c04cb93de9af834869
Mike <spormuv@gmail.com>
2023-02-19 17:01:37 +0300

    feat: fulfill 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const readStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf-8'
+);
+
+readStream.on('data', (chunk) => stdout.write(chunk));
+readStream.on('end', () => console.log('*** End of reading ***'));
+readStream.on('error', (err) => console.error(err.message));
