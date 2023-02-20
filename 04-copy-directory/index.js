df9b4fd9dcf4422c36f71c350e912bf5ea1fc5c9
Mike <spormuv@gmail.com>
2023-02-20 15:28:01 +0300

    feat: fulfill 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const { mkdir, copyFile, readdir, unlink } = require('node:fs/promises');
+const { join } = require('node:path');
 
+const oldDir = join(__dirname, 'files');
+const newDir = join(__dirname, 'files-copy');
+
+const copyDir = async function () {
+  await mkdir(newDir, { recursive: true });
+
+  await readdir(newDir, { withFileTypes: true }).then((data) => {
+    data.forEach((item) =>
+      unlink(join(newDir, item.name), (err) => console.error(err))
+    );
+  });
+
+  await readdir(oldDir, { withFileTypes: true }).then((data) => {
+    data.forEach((item) => {
+      copyFile(join(oldDir, item.name), join(newDir, item.name));
+    });
+  });
+};
+
+copyDir();
